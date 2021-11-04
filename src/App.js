62568be408f4bf11e19678658b559b8c65df1282
robilie <obilie.roberta@gmail.com>
2021-11-03 18:43:39 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Practice Weather App</h1>{" "}
-      <footer>
-        This project was coded by Roberta Obilie and is{" "}
-        <a
-          href="https://github.com/robilie/react-week-5-practice-weather.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Practice Weather App</h1>{" "}
+        <footer>
+          This project was coded by Roberta Obilie and is{" "}
+          <a
+            href="https://github.com/robilie/react-week-5-practice-weather.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
