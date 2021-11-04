923c4478caef909966e1434b3b957a329234ed64
robilie <obilie.roberta@gmail.com>
2021-11-03 17:59:35 -0700

    Added github link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Practice Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Practice Weather App</h1>{" "}
+      <footer>
+        This project was coded by Roberta Obilie and is{" "}
+        <a
+          href="https://github.com/robilie/react-week-5-practice-weather.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
