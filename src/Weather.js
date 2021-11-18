3875b8794123d2049f34eca3099019cd638a23b0
robilie <obilie.roberta@gmail.com>
2021-11-17 19:53:41 -0800

    Added forecast layout with fake data and styling

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
+
 import "./Weather.css";
 
 export default function Weather(props) {
@@ -64,6 +66,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+        <WeatherForecast />
       </div>
     );
   } else {
--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="blue"
-      size={40}
+      size={props.size}
       animate="true"
     />
   );
